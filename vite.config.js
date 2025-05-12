dbcbcbfbe6b2e3664075cb7e283705dd1300f42a
swasthiksoftware <swasthiksoftware@gmail.com>
2025-05-12 05:30:56 -0700

    committing config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/postcss'
+import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [ tailwindcss()
+  plugins: [ react(), tailwindcss()
 ],
 build: {
 outDir: 'dist',
+},
+css: {
+postcss: {
+plugins: [tailwindcss]
+}
 }
 
 })
